51d77e1f2c5b82c60972ed5692d6dd7edf0b953d
marcos-inja <marcosvaas@gmail.com>
2021-06-30 11:18:37 -0300

    ROTAS

--- index.js
+++ index.js
@@ -9,31 +9,31 @@ app.use(express.json())
     app.use(express.static(path.join(__dirname, "client")))
 
 //Rotas usadas para os calculos
-    app.get('/multiplicacao/:n1&:n2', (req, res) => {
+    app.get('/multiplicacao/:n1/:n2', (req, res) => {
         return res.json({ 
                 resultado: calc.mult(req.params.n1, req.params.n2)
             })
     })
 
-    app.get('/soma/:n1&:n2', (req, res) => {
+    app.get('/soma/:n1/n2', (req, res) => {
         return res.json({ 
                 resultado: calc.soma(req.params.n1, req.params.n2)
             })
     })
 
-    app.get('/subtracao/:n1&:n2', (req, res) => {
+    app.get('/subtracao/:n1/:n2', (req, res) => {
         return res.json({ 
                 resultado: calc.sub(req.params.n1, req.params.n2)
             })
     })
 
-    app.get('/divisao/:n1&:n2', (req, res) => {
+    app.get('/divisao/:n1/:n2', (req, res) => {
         return res.json({
                 resultado: calc.div(req.params.n1, req.params.n2)
             })
     })
 
-    app.get('/potencia/:n1&:n2', (req, res) => {
+    app.get('/potencia/:n1/:n2', (req, res) => {
         return res.json({
                 resultado: calc.pot(req.params.n1, req.params.n2)
             })
