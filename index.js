62de12bd730965f2d99f0dd56320312fe5f377e4
marcos-inja <marcosvaas@gmail.com>
2021-07-21 14:39:57 -0300

    principal

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ const app = express()
 const path = require('path')
 const routerCalc = require('./routes/calc')
 
-//Rota principal com o arquivos para a pagina html
-    app.use('/static', express.static(path.resolve(__dirname, 'views', 'static')))
-    app.use(routerCalc)
 
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
-    })
+//Rotas principais com os arquivos para a pagina html
+app.use('/static', express.static(path.resolve(__dirname, 'views', 'static')))
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
+})
 
-    app.get('/sobre', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
-    })
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
+})
 
-    app.get('/documentacao', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
-    })
+app.get('/documentacao', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
+})
 
 //Rotas usadas para os calculos
-    app.use(routerCalc)
+app.use(routerCalc)
 
 let PORTA = 8000
 app.listen(PORTA, () => {
